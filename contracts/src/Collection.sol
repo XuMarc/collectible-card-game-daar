fdc68898834d786649016f571a5b86ab096ca19e
Maarc <xumarc.pro@gmail.com>
2024-10-16 18:34:59 +0200

    add basic contracts

--- contracts/src/Collection.sol
+++ contracts/src/Collection.sol
@@ -1,12 +1,34 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8;
+pragma solidity ^0.8.20;
 
-contract Collection {
-  string public name;
-  int public cardCount;
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 
-  constructor(string memory _name, int _cardCount) {
-    name = _name;
+// Comment ça marche pour nous dans notre tête :
+// ON CRÉE UNE COLLECTION "PIKACHU" 100,
+// ON MINT DES CARTES PIKACHU JUSQUA CE QUON NE PUISSE PLUS (DONC 100 CARTES PIKACHU)
+contract Collection is ERC721URIStorage {
+  // string public name; // NOM DE LA COLLECTTION, NOM DE LA CARTE SPÉCIFIQUE A CETTE COLLECTION (?)
+  uint public cardCount; // = RARETE JE SUPPOSE
+  uint public mintedCount; // TOTAL EN CIRCULATION SUR LA CHAIN
+
+  constructor(string memory _name, uint _cardCount) ERC721(_name, "TCG") {
     cardCount = _cardCount;
+    mintedCount = 0;
+  }
+
+  function mintCard(
+    address to,
+    string memory tokenURI,
+    uint globalCardId
+  ) external {
+    require(
+      mintedCount < cardCount,
+      "ALL CARDS IN THIS COLLECTION ARE ALREADY MINTED"
+    );
+    _mint(to, globalCardId);
+    _setTokenURI(globalCardId, tokenURI);
+
+    mintedCount += 1;
   }
 }
--- contracts/src/Main.sol
+++ contracts/src/Main.sol
@@ -1,17 +1,33 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8;
+pragma solidity ^0.8.20;
 
 import "./Collection.sol";
 
 contract Main {
-  int private count;
-  mapping(int => Collection) private collections;
+  uint private globalCardId; // idcard across all collections, (should it be unique globally or only in a collection ?)
+  uint private count;
+  mapping(uint => Collection) private collections; //hashmap(id,collection);
+
+  address public owner;
+  modifier onlyOwner() {
+    require(msg.sender == owner, "SEUL L'ADMIN PEUT APPELER CETTE FONCTION !");
+    _;
+  }
 
   constructor() {
     count = 0;
+    globalCardId = 1;
+    owner = msg.sender; // Celui qui déploie le contrat(moi) est le owner 
   }
 
-  function createCollection(string calldata name, int cardCount) external {
+  function createCollection(string calldata name, uint cardCount) external onlyOwner{
     collections[count++] = new Collection(name, cardCount);
+  }
+
+  function mint(uint256 collectionId, address to, string memory tokenURI) external onlyOwner {
+    require(collectionId < count, "COLLECTION DOES NOT EXIST");
+    Collection collection = collections[collectionId];
+    collection.mintCard(to, tokenURI, globalCardId);
+    globalCardId++;
   }
 }
